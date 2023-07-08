5dc621bd5cd8c882758ab7022153509670579eee
Ivan Batuhtin <vania.batuhtin@gmail.com>
2023-07-08 16:23:37 +0500

    Editing indents

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css';
-import Form from './components/form'
+import Form from './components/form';
 
 function App() {
-  return (
-    <div className="App">
-      <Form/>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Form />
+		</div>
+	);
 }
 
 export default App;
